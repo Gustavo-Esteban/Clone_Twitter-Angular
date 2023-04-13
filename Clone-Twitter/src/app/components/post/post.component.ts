1e0568f510c66ada1416c0fbef8e210353616004
Estebann <gustavoesteban13@hotmail.com>
2023-04-13 20:41:16 -0300

    Finalizando o clone do twitter

---
+++ Clone-Twitter/src/app/components/post/post.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { MessagesService } from 'src/app/services/messages.service';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+  tweetMessage: string = ""
+  tweetMaxLength: number = 240
+  tweetRemain: number = this.tweetMaxLength
+
+
+  constructor(private service: MessagesService) { }
+
+  ngOnInit(): void {
+  }
+
+  sendTweet(){
+    this.service.addMessage(this.tweetMessage)
+    this.tweetMessage = ""
+    this.tweetRemain = this.tweetMaxLength
+  }
+
+  changeTweet(){
+    this.tweetRemain = this.tweetMaxLength - this.tweetMessage.length
+  }
+
+}
